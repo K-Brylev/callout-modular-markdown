7b153dfdeece18d472fed1953933095596b8308f
K-Brylev <kbrylev@proton.me>
2025-04-27 01:47:27 +0200

    Creation of CSS Snippet and Context Menu addition for icon search

--- main.ts
+++ main.ts
@@ -1,54 +1,37 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, SuggestModal, Notice, Plugin, PluginSettingTab, Setting, getIconIds,setIcon ,getIcon, IconName, TFile } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface CalloutByTagSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: CalloutByTagSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class CalloutByTag extends Plugin {
+	settings: CalloutByTagSettings;
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		await this.createCSSSnippet();
+		this.registerEvent(
+			this.app.workspace.on('editor-menu', (menu, editor, view) => {
+				menu.addItem((item) => {
+					item.setTitle('Insert Icon')
+						.setIcon('list-plus')
+						.setSection('insert')
+						.onClick(() => {
+							new IconModal(this.app).open();
+						});
+				});
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
+		));
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: 'open-icon-modal',
+			name: 'Open Icon Text Select',
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
 				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
@@ -56,7 +39,7 @@ export default class MyPlugin extends Plugin {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						new IconModal(this.app).open();
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -76,6 +59,7 @@ export default class MyPlugin extends Plugin {
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+
 	}
 
 	onunload() {
@@ -89,28 +73,110 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async createCSSSnippet() {
+		const vault = this.app.vault.adapter;
+		const path = '.obsidian/snippets/CalloutByTag.css';
+
+		const colors = ['red', 'blue', 'green', 'cyan', 'purple', 'orange', 'pink'];
+
+		interface Callout {
+			[key: string]: [string[],string];
+		}
+		const baseCallouts:Callout = 
+		{
+			'bug':[['bug'], 'lucide-bug'], 
+			'default':[['default', 'note'],'lucide-pencil'],
+			'error':[['error', 'danger'], 'lucide-zap'],
+			'example':[['example'], 'lucide-list'],
+			'fail':[['fail', 'failure', 'missing'], 'lucide-x'],
+			'important':[['important'],'lucide-flame'],
+			'info':[['info'], 'lucide-info'],
+			'question':[['question', 'help', 'faq'], 'lucide-circle-help'],
+			'success':[['success', 'check','done'], 'lucide-check'],
+			'summary':[['summary', 'abstract', 'tldr'], 'lucide-clipboard-list'],
+			'tip':[['tip', 'hint'], 'lucide-flame'],
+			'todo':[['todo'], 'lucide-circle-check'],
+			'warning':[['warning', 'caution', 'attention'], 'lucide-triangle-alert'],
+			'quote':[['quote', 'cite'], 'lucide-quote'],
+		};
+
+		await vault.write(path, `/* CSS Snippet for CalloutByTag Plugin */\n\n`);
+
+		await vault.append(path, '/*Additional stylings for Callouts*/\n');
+		vault.append(path, 
+			`.callout[data-callout*=\"-darken\"] .callout-content{\n\tbackground-color: #00000020 !important;\n\tborder-radius: var(--callout-radius);\n\tpadding: var(--callout-padding)\n}\n\n`
+		);
+
+		await vault.append(path, '/*Stylings to fix base Callouts*/\n');
+		Object.keys(baseCallouts).forEach(key => {
+			const callout = baseCallouts[key];
+			callout[0].forEach(tag => {
+				vault.append(path, 
+					`.callout[data-callout^=\"${tag}\"] {\n\t--callout-color: var(--callout-${key});\n\t--callout-icon: ${callout[1]};\n}\n\n`
+				);
+			});
+		});
+
+		await vault.append(path, '/*Stylings to change Callout Color*/\n');
+		colors.forEach(color => {
+			vault.append(path, 
+				`.callout[data-callout^=\"${color}\"] {\n\t--callout-color: var(--color-${color}-rgb);\n}\n\n`
+			);
+		});
+
+		await vault.append(path, '/*Stylings to change Callout Icon by using the iconId*/\n');
+		getIconIds().forEach(iconId => {
+			vault.append(path, 
+				`.callout[data-callout*=\"-${iconId}\"] {\n\t--callout-icon: ${iconId};\n}\n\n`
+			);
+		});
+	}
 }
 
-class SampleModal extends Modal {
+class IconModal extends SuggestModal<IconName> {
 	constructor(app: App) {
 		super(app);
 	}
 
+	getSuggestions(query: string): IconName[] {
+		// Filter icons based on the query string
+		return getIconIds().filter((iconId) => iconId.toLowerCase().includes(query.toLowerCase()));
+	}
+
+	renderSuggestion(icon: IconName, el: HTMLElement): void {
+		// Render the icon name and SVG in the suggestion list
+		const suggestionEl = el.createEl('div', );
+		setIcon(suggestionEl, icon);
+		el.createEl('span', { text: icon});
+		suggestionEl.style.display = 'inline-block';
+		suggestionEl.style.marginRight = '8px';
+	}
+
+	onChooseSuggestion(icon: IconName, evt: MouseEvent | KeyboardEvent): void {
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (view) {
+			const cursor = view.editor.getCursor();
+			view.editor.replaceRange(icon, cursor);
+		}
+		// You can also use the icon name directly if needed
+	}
+
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.setText('Search for an icon:');
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: CalloutByTag;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: CalloutByTag) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -131,4 +197,5 @@ class SampleSettingTab extends PluginSettingTab {
 					await this.plugin.saveSettings();
 				}));
 	}
+	
 }
